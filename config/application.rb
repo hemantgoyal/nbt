fa78787067cedc076a9234ebf35b6c1b403c098a
Anbazhagan <anbublacky@gmail.com>
2013-08-17 23:14:52 +0530

    changes to make css work

--- config/application.rb
+++ config/application.rb
@@ -64,7 +64,7 @@ module Nabblog1
 
     # Enable the asset pipeline
     config.assets.enabled = true
-    #config.assets.paths << "#{Rails.root}/public/assets"
+    config.assets.paths << "#{Rails.root}/public/assets"
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
