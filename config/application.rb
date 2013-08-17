c4ff4bf6108a74d673d8be882b578dc4792f23bf
Anbazhagan <anbublacky@gmail.com>
2013-08-17 21:44:29 +0530

    changes to application still psych error exist

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,7 @@ module Nabblog1
     # -- all .rb files in that directory are automatically loaded.
 
     # Custom directories with classes and modules you want to be autoloadable.
-     config.autoload_paths += %W(#{config.root}/lib)
+     #config.autoload_paths += %W(#{config.root}/lib)
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
@@ -64,7 +64,7 @@ module Nabblog1
 
     # Enable the asset pipeline
     config.assets.enabled = true
-    config.assets.paths << "#{Rails.root}/public/assets"
+    #config.assets.paths << "#{Rails.root}/public/assets"
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
