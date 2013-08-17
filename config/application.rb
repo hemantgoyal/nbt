11120ab19a3aa6da02915e4ad54fd2b718e777bc
Anbazhagan <anbublacky@gmail.com>
2013-08-17 19:50:54 +0530

    changes to make assets work

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ require "active_resource/railtie"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
-Bundler.require(:default, Rails.env) if defined?(Bundler)
+Bundler.require(:default, :assets, Rails.env) if defined?(Bundler)
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
