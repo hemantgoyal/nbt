e9d52e47cd77962f4705b5ea7b9fb4e77bd05ab8
Anbazhagan <anbublacky@gmail.com>
2013-08-19 15:40:41 +0530

    changes to home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-        @pages = Refinery::Page.live.limit(3)
+        @pages = Refinery::Page.live.order("created_at DESC").limit(3)
   end
 end
