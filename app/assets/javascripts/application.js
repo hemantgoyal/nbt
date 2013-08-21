3ef1fdd45b8ff60a92febad1aadfc1a501fd560f
Anbazhagan <anbublacky@gmail.com>
2013-08-21 16:12:54 +0530

    changes to mailchimp

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,10 +20,16 @@
 //= require_tree .
 
 $(document).ready(function() {
+      var columns    = 3,
+        setColumns = function() { columns = $( window ).width() > 640 ? 3 : $( window ).width() > 320 ? 2 : 1; };
+ 
+    setColumns();
+    $( window ).resize( setColumns );
+ 
     $( '#list' ).masonry(
     {
-        itemSelector: '.item'
-    });
+        itemSelector: '.item',
+        columnWidth:  function( containerWidth ) { return containerWidth / columns; }
 });
 $(document).foundation();
   
