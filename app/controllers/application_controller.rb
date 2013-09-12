a091277d8ac4db65bbe635aad14b2253270b5c3a
Anbazhagan <anbublacky@gmail.com>
2013-09-13 00:17:13 +0530

    changes to application controller host

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-
-
-
+  
+    def default_url_options
+    if Rails.env.production?
+      {:host => "nabthat.com:3000"}
+    else  
+      {}
+    end
+  end
+  
 end
