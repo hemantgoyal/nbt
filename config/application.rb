099f4b50e53b796f4eda57546557ecf943b3b72e
Anbazhagan <anbublacky@gmail.com>
2013-09-12 11:26:35 +0530

    changes to application

--- config/application.rb
+++ config/application.rb
@@ -65,7 +65,7 @@ module Nabblog1
 
     # Enable the asset pipeline
     config.assets.enabled = true
-    config.assets.initialize_on_precompile = true
+    config.assets.initialize_on_precompile = false
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
