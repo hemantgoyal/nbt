8a5a72f6d86c67acd6e27d2edc7401d152aa98c5
hemantgoyal <hemantgoyal1991@gmail.com>
2013-09-13 20:54:27 +0530

    changed too github repo

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,154 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130815085447) do
+ActiveRecord::Schema.define(:version => 20130906093721) do
+
+  create_table "bids", :force => true do |t|
+    t.integer  "user_id"
+    t.boolean  "buy"
+    t.boolean  "lease"
+    t.boolean  "finance"
+    t.integer  "monthly_payment"
+    t.integer  "down_payment"
+    t.integer  "no_of_month"
+    t.string   "comment"
+    t.boolean  "save_bid"
+    t.integer  "max_miles"
+    t.integer  "bid_time"
+    t.integer  "customer_id"
+    t.integer  "dealer_id"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+  end
+
+  create_table "car_infos", :force => true do |t|
+    t.integer  "user_id"
+    t.string   "make"
+    t.string   "model"
+    t.date     "model_year"
+    t.string   "trim"
+    t.string   "additional_feature"
+    t.string   "car_image"
+    t.string   "body_style"
+    t.integer  "price"
+    t.integer  "mileage"
+    t.string   "exterior_color"
+    t.string   "interior_color"
+    t.string   "transmission"
+    t.string   "engine"
+    t.datetime "created_at",         :null => false
+    t.datetime "updated_at",         :null => false
+  end
+
+  create_table "ckeditor_assets", :force => true do |t|
+    t.string   "data_file_name",                  :null => false
+    t.string   "data_content_type"
+    t.integer  "data_file_size"
+    t.integer  "assetable_id"
+    t.string   "assetable_type",    :limit => 30
+    t.string   "type",              :limit => 30
+    t.integer  "width"
+    t.integer  "height"
+    t.datetime "created_at",                      :null => false
+    t.datetime "updated_at",                      :null => false
+  end
+
+  add_index "ckeditor_assets", ["assetable_type", "assetable_id"], :name => "idx_ckeditor_assetable"
+  add_index "ckeditor_assets", ["assetable_type", "type", "assetable_id"], :name => "idx_ckeditor_assetable_type"
+
+  create_table "counter_bids", :force => true do |t|
+    t.integer  "user_id"
+    t.integer  "bid_id"
+    t.boolean  "buy"
+    t.boolean  "lease"
+    t.boolean  "finance"
+    t.integer  "monthly_payment"
+    t.integer  "down_payment"
+    t.integer  "no_of_month"
+    t.string   "comment"
+    t.boolean  "save_cbid"
+    t.integer  "max_miles"
+    t.integer  "bid_time"
+    t.integer  "confirmation_no"
+    t.integer  "dealer_id"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+  end
+
+  create_table "maillinglists", :force => true do |t|
+    t.string   "email"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "profiles", :force => true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.datetime "dob"
+    t.string   "image"
+    t.string   "street"
+    t.string   "city"
+    t.string   "state"
+    t.integer  "zipcode"
+    t.integer  "phone"
+    t.integer  "fax"
+    t.integer  "social_security_no"
+    t.integer  "user_id"
+    t.datetime "created_at",         :null => false
+    t.datetime "updated_at",         :null => false
+  end
+
+  create_table "refinery_blog_categories", :force => true do |t|
+    t.string   "title"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+    t.string   "cached_slug"
+    t.string   "slug"
+  end
+
+  add_index "refinery_blog_categories", ["id"], :name => "index_refinery_blog_categories_on_id"
+  add_index "refinery_blog_categories", ["slug"], :name => "index_refinery_blog_categories_on_slug"
+
+  create_table "refinery_blog_categories_blog_posts", :force => true do |t|
+    t.integer "blog_category_id"
+    t.integer "blog_post_id"
+  end
+
+  add_index "refinery_blog_categories_blog_posts", ["blog_category_id", "blog_post_id"], :name => "index_blog_categories_blog_posts_on_bc_and_bp"
+
+  create_table "refinery_blog_comments", :force => true do |t|
+    t.integer  "blog_post_id"
+    t.boolean  "spam"
+    t.string   "name"
+    t.string   "email"
+    t.text     "body"
+    t.string   "state"
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
+  end
+
+  add_index "refinery_blog_comments", ["id"], :name => "index_refinery_blog_comments_on_id"
+
+  create_table "refinery_blog_posts", :force => true do |t|
+    t.string   "title"
+    t.text     "body"
+    t.boolean  "draft"
+    t.datetime "published_at"
+    t.datetime "created_at",                      :null => false
+    t.datetime "updated_at",                      :null => false
+    t.integer  "user_id"
+    t.string   "cached_slug"
+    t.string   "custom_url"
+    t.text     "custom_teaser"
+    t.string   "source_url"
+    t.string   "source_url_title"
+    t.integer  "access_count",     :default => 0
+    t.string   "slug"
+  end
+
+  add_index "refinery_blog_posts", ["access_count"], :name => "index_refinery_blog_posts_on_access_count"
+  add_index "refinery_blog_posts", ["id"], :name => "index_refinery_blog_posts_on_id"
+  add_index "refinery_blog_posts", ["slug"], :name => "index_refinery_blog_posts_on_slug"
 
   create_table "refinery_images", :force => true do |t|
     t.string   "image_mime_type"
@@ -126,6 +273,19 @@ ActiveRecord::Schema.define(:version => 20130815085447) do
   add_index "refinery_roles_users", ["role_id", "user_id"], :name => "index_refinery_roles_users_on_role_id_and_user_id"
   add_index "refinery_roles_users", ["user_id", "role_id"], :name => "index_refinery_roles_users_on_user_id_and_role_id"
 
+  create_table "refinery_settings", :force => true do |t|
+    t.string   "name"
+    t.text     "value"
+    t.boolean  "destroyable",     :default => true
+    t.string   "scoping"
+    t.boolean  "restricted",      :default => false
+    t.string   "form_value_type"
+    t.datetime "created_at",                         :null => false
+    t.datetime "updated_at",                         :null => false
+  end
+
+  add_index "refinery_settings", ["name"], :name => "index_refinery_settings_on_name"
+
   create_table "refinery_user_plugins", :force => true do |t|
     t.integer "user_id"
     t.string  "name"
@@ -189,5 +349,49 @@ ActiveRecord::Schema.define(:version => 20130815085447) do
 
   add_index "seo_meta", ["id"], :name => "index_seo_meta_on_id"
   add_index "seo_meta", ["seo_meta_id", "seo_meta_type"], :name => "index_seo_meta_on_seo_meta_id_and_seo_meta_type"
+
+  create_table "taggings", :force => true do |t|
+    t.integer  "tag_id"
+    t.integer  "taggable_id"
+    t.string   "taggable_type"
+    t.integer  "tagger_id"
+    t.string   "tagger_type"
+    t.string   "context"
+    t.datetime "created_at"
+  end
+
+  add_index "taggings", ["tag_id"], :name => "index_taggings_on_tag_id"
+  add_index "taggings", ["taggable_id", "taggable_type", "context"], :name => "index_taggings_on_taggable_id_and_taggable_type_and_context"
+
+  create_table "tags", :force => true do |t|
+    t.string "name"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "email",                  :default => "", :null => false
+    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
+    t.datetime "created_at",                             :null => false
+    t.datetime "updated_at",                             :null => false
+    t.string   "role"
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "name"
+  end
+
+  add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
 end
