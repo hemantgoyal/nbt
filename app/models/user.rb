00c405aeaa2205c79dd39efc2cf0b2128723b0bd
Anbazhagan <anbublacky@gmail.com>
2013-09-13 11:39:39 +0530

    changes to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -52,6 +52,10 @@ class User < ActiveRecord::Base
                          email:auth.info.email,
                          password:Devise.friendly_token[0,20]
                          )
+    user.skip_confirmation!
+    user.save!
+    user
+    
   end
   user
 end
@@ -71,26 +75,10 @@ def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
           uid: access_token.uid ,
           password: Devise.friendly_token[0,20],
         )
-      end
-   end
-end
-
-def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
-    data = access_token.info
-    user = User.where(:provider => access_token.provider, :uid => access_token.uid ).first
-    if user
-      return user
-    else
-      registered_user = User.where(:email => access_token.info.email).first
-      if registered_user
-        return registered_user
-      else
-        user = User.create(name: data["name"],
-          provider:access_token.provider,
-          email: data["email"],
-          uid: access_token.uid ,
-          password: Devise.friendly_token[0,20],
-        )
+        user.skip_confirmation!
+        user.save!
+        user
+        
       end
    end
 end
@@ -111,6 +99,9 @@ def self.find_for_twitter_oauth(auth, signed_in_resource=nil)
         email:auth.uid+"@twitter.com",
         password:Devise.friendly_token[0,20],
       )
+      user.skip_confirmation!
+      user.save!
+      user
     end
   end
 end
